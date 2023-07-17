773dab17eefc61668eb89bb71171f961f24d29fb
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-07-17 16:37:46 +0300

    com9

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -20,7 +20,7 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 }
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.array,     
+    options: PropTypes.arrayOf(PropTypes.string,),     
     onLeaveFeedback: PropTypes.func,
     
 }
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,7 +1,13 @@
-export const Notification = ({message}) => {
+import PropTypes from "prop-types";
+
+export const Notification = ({ message }) => {
     return (
         <div>
             <p>{message}</p>
         </div>
     )
+}
+
+Notification.propTypes = {
+    message: PropTypes.string,
 }
